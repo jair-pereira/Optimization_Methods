0fd7335366d889a5504c25abb915ed9f7b4449d8
Jair Pereira <jair.pereira@aluno.ufabc.edu.br>
2019-03-28 14:58:05 +0900

    update solvers

--- solvers.py
+++ solvers.py
@@ -77,7 +77,7 @@ def cs(n, my_func, bounds, dimension, max_nfe, pr, k):
         [Xi.getFitness() for Xi in X]
     return Solution
     
-def f201(n, my_func, bounds, dimension, max_nfe):#n=100
+def f201(n, my_func, bounds, dimension, max_nfe, r1w, r1c1, r1c2, r2k, r2beta, r3w, r3c1, r3c2):
     Solution.setProblem(my_func, bounds, dimension, maximize=False)
     Solution.repair = op.repair_random
     X = Solution.initialize(n)
@@ -88,22 +88,22 @@ def f201(n, my_func, bounds, dimension, max_nfe):#n=100
         U = X
         #Round 1
         S1 = op.select_current(U)
-        U = op.w_pso(S1, w=0.75, c1=0.25, c2=1.00)
+        U = op.w_pso(S1, w, c1, c2)
         X  = op.replace_if_random(X, U)
         #Round 2
-        S1 = op.select_tournament(U, n=1, k=int(n*0.50))
+        S1 = op.select_tournament(U, n=1, k)
         S2 = op.select_random(X, 1)
         S3 = op.select_current(X)
-        U  = op.w_mut_de(S1, S2, S3, beta=0.50)
+        U  = op.w_mut_de(S1, S2, S3, beta)
         X  = op.replace_if_best(X, U)
         #Round 3
         S1 = op.select_current(U)
-        U  = op.w_pso(S1, w=0.50, c1=0.25, c2=0.00)
+        U  = op.w_pso(S1, w, c1, c2)
         X  = op.replace_if_random(X, U)
         [Xi.getFitness() for Xi in X]
     return X
     
-def f203(n, my_func, bounds, dimension, max_nfe):#n=100
+def f203(n, my_func, bounds, dimension, max_nfe, k, pr, beta):
     Solution.setProblem(my_func, bounds, dimension, maximize=False)
     Solution.repair = op.repair_truncate
     X = Solution.initialize(n)
@@ -113,20 +113,20 @@ def f203(n, my_func, bounds, dimension, max_nfe):#n=100
     while Solution.nfe < max_nfe:
         U = X
         #Round 1
-        S1 = op.select_tournament(U, n=1, k=int(n*0.25))
+        S1 = op.select_tournament(U, n=1, k)
         S2 = op.select_random(X, 1)
-        U  = op.w_crx_exp2(S1, S2, pr=0.75)
+        U  = op.w_crx_exp2(S1, S2, pr)
         X  = op.replace_if_random(X, U)
         #Round 2
         S1 = op.select_random(U, 1)
         S2 = op.select_random(X, 1)
         S3 = op.select_current(U)
-        U  = op.w_mut_de(S1, S2, S3, beta=0.50)
+        U  = op.w_mut_de(S1, S2, S3, beta)
         X  = U
         [Xi.getFitness() for Xi in X]
     return X
     
-def f251(n, my_func, bounds, dimension, max_nfe):#n=50
+def f251(n, my_func, bounds, dimension, max_nfe, w, c1, c2, pr):
     Solution.setProblem(my_func, bounds, dimension, maximize=False)
     Solution.repair = op.repair_random
     X = Solution.initialize(n)
@@ -137,14 +137,14 @@ def f251(n, my_func, bounds, dimension, max_nfe):#n=50
         U = X
         #Round 1
         S1 = op.select_current(X)
-        U  = op.w_pso(S1, w=0.50, c1=0.00, c2=2.00)
+        U  = op.w_pso(S1, w, c1, c2)
         X  = op.replace_if_random(X, U)
         #Round Drop
-        X = op.drop_probability(X, pr=0.25)
+        X = op.drop_probability(X, pr)
         [Xi.getFitness() for Xi in X]
     return X
     
-def f253(n, my_func, bounds, dimension, max_nfe):#n=200
+def f253(n, my_func, bounds, dimension, max_nfe, w, c1, c2):
     Solution.setProblem(my_func, bounds, dimension, maximize=False)
     Solution.repair = op.repair_random
     X = Solution.initialize(n)
@@ -155,7 +155,7 @@ def f253(n, my_func, bounds, dimension, max_nfe):#n=200
         U = X
         #Round 1
         S1 = op.select_random(U, 1)
-        U  = op.w_pso(S1, w=0.00, c1=0.00, c2=2.00)
+        U  = op.w_pso(S1, w, c1, c2)
         X  = U
         [Xi.getFitness() for Xi in X]
-    return X    
+    return X
